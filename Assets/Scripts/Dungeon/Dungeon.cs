4db56f1d837ed422cf226a7c9de06b67bb3be3d6
aymericdefonclare-hub <aymericdefonclare@gmail.com>
2025-10-24 23:14:18 +0200

    Added Range for Attributes of  Dungeon ScriptableObjects

--- Assets/Scripts/Dungeon/Dungeon.cs
+++ Assets/Scripts/Dungeon/Dungeon.cs
@@ -6,4 +6,16 @@ public class Dungeon : ScriptableObject
     public string dungeonName;
     public Sprite dungeonIcon;
     public string dungeonDescription;
+
+    [Range(0, 100)]
+    public int attribute1 = 10;
+
+    [Range(0, 100)]
+    public int attribute2 = 10;
+
+    [Range(0, 100)]
+    public int attribute3 = 10;
+
+    [Range(0, 100)]
+    public int attribute4 = 10;
 }
