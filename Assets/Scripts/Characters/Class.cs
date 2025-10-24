749bbf4f2f470377c56a137388845cc4cbf4ec9a
Heeko2b <jacques.battaglini@hotmail.fr>
2025-10-24 21:54:23 +0200

    Add classes & attributes
    Introduces ScriptableObject definitions for character classes and attributes, including assets for various classes (Barbarian, Bard, Paladin, etc.) and their metadata.

---
+++ Assets/Scripts/Characters/Class.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Class", menuName = "Scriptable Objects/Class")]
+public class Class : ScriptableObject
+{
+    public Sprite classIcon;
+    public Attribute atribute1;
+    public Attribute atribute2;
+    public Attribute atribute3;
+}
