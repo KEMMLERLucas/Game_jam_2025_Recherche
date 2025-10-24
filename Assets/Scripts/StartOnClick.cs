6c388d347d8b2d4768438e2e0a7be6bd8522efe3
aymericdefonclare-hub <aymericdefonclare@gmail.com>
2025-10-24 21:43:59 +0200

    Added a MainMenu Scene
    added a main menu with a title and 2 buttons start and quit

---
+++ Assets/Scripts/StartOnClick.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartOnClickScript : MonoBehaviour
+{    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
