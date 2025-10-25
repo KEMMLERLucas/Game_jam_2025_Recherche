a13a00b3e9c62755ded6f7b8f84582ea6ca02dd4
aymericdefonclare-hub <aymericdefonclare@gmail.com>
2025-10-25 12:30:55 +0200

    Modified ScoreManager Script & score GameObject

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,8 +1,12 @@
+using TMPro;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
+    public TextMeshProUGUI scoreText;
+
     int score;
+    int currentScore;
     void Start()
     {
         score = 0;
@@ -10,7 +14,10 @@ public class ScoreManager : MonoBehaviour
 
    public void OnTeamSelectionResolution()
     {
+        // comparer la liste de perso avec les requirements du donjon
 
+        score = currentScore;
+        scoreText.text = "score : " + score;
     }
 
 }
