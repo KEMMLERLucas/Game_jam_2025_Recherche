24f3093ddaa62e7d0d7a6263068f3ab957dcb5eb
Heeko2b <jacques.battaglini@hotmail.fr>
2025-10-25 13:19:05 +0200

    Uncommented the dungeonName
    Uncommented the dungeonName field in the Dungeon ScriptableObject to make it accessible. This allows the dungeon name to be set and used in the Unity editor and code.

--- Assets/Scripts/Dungeon/Dungeon.cs
+++ Assets/Scripts/Dungeon/Dungeon.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Dungeon", menuName = "Scriptable Objects/Dungeon")]
 public class Dungeon : ScriptableObject
 {
-    //public string dungeonName;
+    public string dungeonName;
     public Sprite dungeonIcon;
     //public string dungeonDescription;
     public Attribute attribute1;
